48b10702936dbafafb2fd1457a509d45b0c39a22
Lukin Egor <lukin.net@gmail.com>
2022-08-21 16:48:35 +0400

    add base spec for counter cache interface

--- spec/support/active_record_init.rb
+++ spec/support/active_record_init.rb
@@ -3,7 +3,17 @@
 ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
 
 ActiveRecord::Schema.define do
-  # define schema here
+  create_table "articles", force: true do |t|
+    t.integer  "comments_count", default: 0, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 end
 
 ActiveRecord::Base.logger = Logger.new($stdout) if ENV["LOG"]
