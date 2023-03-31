cade36bade6e253446910380b116936468b98701
Egor Lukin <lukin.net@gmail.com>
2023-03-31 18:49:47 +0400

    refactor: add flexible structure for new adapters

---
+++ lib/activerecord_slotted_counters/adapters/rails_upsert.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module ActiveRecordSlottedCounters
+  module Adapters
+    class RailsUpsert
+      attr_reader :klass
+
+      def initialize(klass)
+        @klass = klass
+      end
+
+      def apply?
+        ActiveRecord::VERSION::MAJOR >= 7
+      end
+
+      def bulk_insert(attributes, on_duplicate: nil, unique_by: nil)
+        klass.upsert_all(attributes, on_duplicate: on_duplicate, unique_by: unique_by)
+      end
+    end
+  end
+end
